aaae475380972f5f48bf1d7bb937fdb54c13a7de
tycsbs <tutebaisong@163.com>
2017-03-27 15:17:43 +0800

    add chapter and chapterSource entity
    add two class

---
+++ Modle/Chapter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modle
+{
+    [Serializable]
+    public class Chapter
+    {
+        public Chapter()
+        { }
+        #region Model
+        private int _chid;
+        private int _cid;
+        private string _chname;
+        private DateTime? _starttime = DateTime.Now;
+        private string _mark;
+        private int? _isdel = 0;
+        /// <summary>
+        /// 
+        /// </summary>
+        public int chId
+        {
+            set { _chid = value; }
+            get { return _chid; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int cId
+        {
+            set { _cid = value; }
+            get { return _cid; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string chName
+        {
+            set { _chname = value; }
+            get { return _chname; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime? starttime
+        {
+            set { _starttime = value; }
+            get { return _starttime; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string mark
+        {
+            set { _mark = value; }
+            get { return _mark; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int? isDel
+        {
+            set { _isdel = value; }
+            get { return _isdel; }
+        }
+        #endregion Model
+
+    }
+}
---
+++ Modle/ChapterSource.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modle
+{
+    [Serializable]
+    public class ChapterSource
+    {
+        public ChapterSource()
+        { }
+        #region Model
+        private int _chid;
+        private string _chname;
+        private string _srcurl;
+        private string _srctype;
+        private int? _isdel = 0;
+        private DateTime? _timestamp = DateTime.Now;
+        /// <summary>
+        /// 
+        /// </summary>
+        public int chId
+        {
+            set { _chid = value; }
+            get { return _chid; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string chName
+        {
+            set { _chname = value; }
+            get { return _chname; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string srcUrl
+        {
+            set { _srcurl = value; }
+            get { return _srcurl; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string srcType
+        {
+            set { _srctype = value; }
+            get { return _srctype; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int? isDel
+        {
+            set { _isdel = value; }
+            get { return _isdel; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime? timeStamp
+        {
+            set { _timestamp = value; }
+            get { return _timestamp; }
+        }
+        #endregion Model
+    }
+}
