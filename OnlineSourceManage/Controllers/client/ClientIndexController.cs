adf0c1f6d11be1e7f8d02e19ce538265c8974eb8
tycsbs <tutebaisong@163.com>
2017-04-06 23:48:43 +0800

    add clientIndex page
    made clientpage done

---
+++ OnlineSourceManage/Controllers/client/ClientIndexController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BLL;
+using Modle;
+
+namespace OnlineSourceManage.Controllers.client
+{
+    public class ClientIndexController : Controller
+    {
+        //
+        // GET: /ClientIndex/
+        private readonly CourseBll _bll = new CourseBll();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetCourseToNav()
+        {
+            DataTable dt = _bll.GetCourseByTypesOrLevel("types");
+            List<int> ints = new List<int>();
+            List<string> strList = new List<string>();
+            foreach (DataRow row in dt.Rows)
+            {
+                strList.Add(row[0].ToString());
+                ints.Add(Convert.ToInt32(row[1]));
+            }
+            return Json(new {types = strList, numlist = ints},JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// 获取所有课程信息
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetDefaultCart()
+        {
+           List<Course> list = _bll.GetAllCourse(true);
+            return Json(new {total = list.Count, rows = list}, JsonRequestBehavior.AllowGet);
+        }
+        /// <summary>
+        /// 查找不同类型的课程
+        /// </summary>
+        /// <param name="types"></param>
+        /// <returns></returns>
+        public ActionResult GetCartByTypes(string types)
+        {
+            List<Course> list = _bll.GetCouseByTypes(types);
+            return Json(new { total = list.Count, rows = list }, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+
+
+    }
+}
--- OnlineSourceManage/Scripts/Js/clientIndex.js
+++ OnlineSourceManage/Scripts/Js/clientIndex.js
@@ -1,12 +1,66 @@
 /**
  * Created by DELL on 2017/4/6.
  */
-$(function(){
-    //加载卡片界面的渐变背景样式
-    LoadCardCss();
+$(function () {
+    //nav 加载
+    LoadNavData();
+    //加载默认卡片内容
+    LoadCartData("/ClientIndex/GetDefaultCart", {});
+    //获取不同类型的课程
+    $("#nav-container").on('click', 'li', function () {
+        var keys = $(this).children('a').data("key");
+        LoadCartData("/ClientIndex/GetCartByTypes", { types: keys });
+    });
+
 });
 
+//加载导航条数据
+function LoadNavData() {
+    var iconMap = ["fa-desktop", "fa-codepen", "fa-picture-o", "fa-database", "fa-tablet", "fa-cloud-upload", "fa-cogs"];
+    $.ajax({
+        url: "/ClientIndex/GetCourseToNav",
+        success: function (d) {
+            var intlist = d.numlist, strlist = d.types, len = strlist.length;
+            $("#nav-container ul").empty();
+            for (var i = 0; i < len; i++) {
+                var s = '<li> <span class="icon"><i class="fa ' + iconMap[i] + '"></i></span>' +
+                    '<a href="javascript:;" data-key="' + strlist[i] +'" ><p class="nav-title">' + strlist[i] +
+                    '</p><i class="course-num">' +intlist[i] +' 门课程</i></a></li>';
+                $("#nav-container ul").append(s);
+            }
 
+        }
+    });
+
+}
+/**
+ * 加载卡片数据
+ * @param {} url 
+ * @param {} parm 
+ * @returns {} none 
+ */
+function LoadCartData(url, parm) {
+    $.ajax({
+        url: url,
+        data: parm,
+        success: function (d) {
+            var levelMap = ["初级", "中级", "高级"];
+            var count = d.total, data = d.rows;
+            var html = "";
+            $.each(data, function (i, val) {
+                html += '<div class="col-md-3"><div class="course-wrapper"><a target="_blank" class="course-card" href="javascript:;">'
+                   + '<div class="course-card-top"><i class="fa fa-play-circle"></i><span>'
+                   + val.types + '</span></div><div class="course-card-content"><h3 class="course-card-name">'
+                   + val.cName + '</h3><p>' + val.mark + '</p><div class="clearfix course-card-bottom"><div class="course-card-info">'
+                   + levelMap[val.levelNum] + ' <span>·</span> ' + val.startTime.substr(0, val.startTime.length - 7) + '</div></div></div></a></div></div>';
+            });
+            $("#container").empty().append(html);
+
+            //加载卡片界面的渐变背景样式
+            LoadCardCss();
+        }
+    });
+}
 
 
 /**
@@ -14,8 +68,8 @@ $(function(){
  * @constructor none
  */
 function LoadCardCss() {
-    var classMap = ["purple","orange","green","blue"];
-    $(".course-card-top").each(function(i,v){
+    var classMap = ["purple", "orange", "green", "blue"];
+    $(".course-card-top").each(function (i, v) {
         $(v).addClass(classMap[i % classMap.length]);
     });
 }
